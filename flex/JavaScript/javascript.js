14fec5dd32b6dc30a7cdb9cb2a3665ca1f65b93f
Alex1721 <73285589+Alex1721@users.noreply.github.com>
2023-07-26 22:31:44 +0200

    Rock-paper-scissors

--- flex/JavaScript/javascript.js
+++
@@ -1,7 +0,0 @@
-const firstName = "Alexandre";
-const lastName = "Boving";
-const thisYear = 2023;
-const birthYear = 1999;
-const greeting = "Hi, my name is " + firstName + " " + lastName + ". And I was born in " + birthYear + "." + " I'm thus, " + (thisYear - birthYear) + " years old.";
-
-console.log(greeting);
